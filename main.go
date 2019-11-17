15409ee53b823e2944b2d85a26db8102b2beba9f
benjaminr177 <unoceptik@gmail.com>
2019-11-17 11:49:59 +0000

    create child process inside main process with own namespace etc

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// +build linux
+
 package main
 
 import (
@@ -12,28 +14,48 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("whaat??")
 	}
 }
 
 func run() {
-	fmt.Printf("got %v\n", os.Args[2:])
-
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	fmt.Printf("%v\n", os.Getpid())
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 
 	err := cmd.Run()
 	if err != nil {
-		must(err)
+		panic(err)
 	}
 }
 
-func must(err error) {
-	panic(err)
+func child() {
+	fmt.Printf("%v\n", os.Getpid())
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := syscall.Chroot("/home/rootfs")
+	checkError(err)
+	err = os.Chdir("/")
+	checkError(err)
+	err = syscall.Mount("proc", "proc", "proc", 0, "")
+	checkError(err)
+	err = cmd.Run()
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
 }
